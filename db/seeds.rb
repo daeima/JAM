112ce6c979a71ee441502d513dd08e571402873b
Cristina Barbieri <daeima87@gmail.com>
2021-11-23 14:10:53 +0100

    Temporary seeds - User, JobApplication, Interview, Contact

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  emial: "user@user.com",
+  password: "password",
+  first_name: "John",
+  last_name: "Wick"
+)
+
+JobApplication.create!(
+  title: "Frontend Engineer",
+  company_name: "Backbase",
+  description: "As a Frontend Engineer you'll be using Angular and TypeScript to create the best web interfaces, administration panels, and omni-channel user experiences.
+
+We love clean, testable, and maintainable code that is cross-browser compatible. You will be part of a multidisciplinary team that is end-to-end responsible for analysis, design, implementation and delivery of production-ready software.
+
+We like to keep work interesting and we encourage our developers to develop their personal strengths."
+)
+
+Interview.create!(
+  start_date: "2021-11-24 14:00:00",
+  end_date: "2021-11-24 15:00:00"
+)
+
+Contact.create!(
+  first_name: "Viggo",
+  last_name: "Tarasov",
+  position: "Mob Boss",
+  email: "viggoTheBoss@hotmail.com"
+)

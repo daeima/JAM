f228746a99e448a93f9a2a6afe97a4c062b9103f
Cristina Barbieri <daeima87@gmail.com>
2021-11-23 12:08:04 +0100

    Routes made

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :job_applications, except: %i[destroy] do
+    resources :interviews, except: %i[index] do
+      resources :contacts, only: %i[new create]
+    end
+  end
 end

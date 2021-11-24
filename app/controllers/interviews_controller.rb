500874e75be6ec315470212037cb08d291d0ab60
massinissa aouchiche <aouchiche.massinissa@gmail.com>
2021-11-24 14:33:39 +0100

    created a new interview and display interviews

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,25 +1,26 @@
+
 class InterviewsController < ApplicationController
+
+  
+  def new
+  @interview = Interview.new
+  @job_application = JobApplication.find(params[:job_application_id])
+  end
 
   def index
     @interviews = Interview.all
   end
-
-  def new
-    @interview = Interview.new
-    @job_application = JobApplication.find(params[:job_application_id])
-  end
+  
 
   def create
     @interview = Interview.new(interview_params)
-    @interview.save
-    
-      @interview.job_application = Venue.find(params[:venue_id])
-      if @review.save!
-        redirect_to review_path(@review)
+    @interview.job_application = JobApplication.find(params[:job_application_id])
+    if @interview.save!
+        redirect_to interview_path(@interview)
       else
-        render 'job_application/show'
-      end
+        render :new
     end
+  
   
   end
 
@@ -28,4 +29,13 @@ class InterviewsController < ApplicationController
   def interview_params
     params.require(:interview).permit(:notes, :start_date, :end_date, :link, :interview_type, :job_application)
   end
+ 
 end
+
+
+
+
+
+
+
+

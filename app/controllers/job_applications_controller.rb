c0ad0030c246b292355f6b7af3fdd4dc9e7b0129
Robert Kutt <robert.kutt@gmail.com>
2021-11-24 12:30:53 +0100

    started search method

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,7 +1,18 @@
 class JobApplicationsController < ApplicationController
 
   def index
-    @job_applications = JobApplication.all
+    def index
+    @job_applications = JobApplication.order(title: :desc)
+
+    if params[:query].present?
+      @job_applications = @job_applications.where('title ILIKE ?', "%#{params[:query]}%")
+    end
+
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: 'list.html', locals: { JobApplications: @job_applications } }
+    end
+    end
   end
 
   def show
--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -3,4 +3,11 @@ class JobApplication < ApplicationRecord
   has_many :interviews, dependent: :destroy
 
   validates :title, :company_name, presence: true
+
+    include PgSearch::Model
+    pg_search_scope :search_by_title_and_company_name,
+      against: [ :title, :company_name ],
+      using: {
+        tsearch: { prefix: true }
+      }
 end

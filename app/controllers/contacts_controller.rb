9ffc5e3f5185e4857e646a2570e9369ee3ab92ed
Cristina Barbieri <daeima87@gmail.com>
2021-11-24 17:36:52 +0100

    Added contact to interview

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,20 @@
 class ContactsController < ApplicationController
+
+  def create
+    @interview = Interview.find(params[:interview_id])
+    @contact = Contact.new(conatct_params)
+    @contact.interview = @interview
+
+    if @contact.save
+      redirect_to job_application_path(@interview.job_application)
+    else
+      render 'job_applications/show'
+    end
+  end
+
+  private
+
+  def conatct_params
+    params.require(:contact).permit(:first_name, :last_name, :position, :email, :phone, :contact_url, :interview)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :job_applications, except: %i[destroy] do
     resources :interviews, except: %i[index] do
-      resources :contacts, only: %i[new create]
     end
+  end
+  resources :interviews, only: [] do
+    resources :contacts, only: %i[create]
   end
 
   if Rails.env.development?
-    get 'kitchensink', to: 'pages#kitchensink' 
+    get 'kitchensink', to: 'pages#kitchensink'
   end
 end

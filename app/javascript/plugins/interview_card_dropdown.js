e2b951d95f2f378cba474e5593f56ff572ab18c0
Cristina Barbieri <daeima87@gmail.com>
2021-11-30 22:10:07 +0100

    cleaned interview card, added favorite link, edit job link and created modal for add interview

--- app/javascript/plugins/interview_card_dropdown.js
+++ app/javascript/plugins/interview_card_dropdown.js
@@ -1,10 +1,9 @@
 function drop() {
   const acc = document.getElementsByClassName("more");
-  const i;
   for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
       this.classList.toggle("active");
-      const panel = document.getElementsByClassName("panel");
+      const panel = document.getElementsByClassName("interview-panel");
       if (panel.style.display === "block") {
         panel.style.display = "none";
       } else {

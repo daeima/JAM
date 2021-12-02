02a001ffea31bcbb8d479723438e367024aa54b6
massinissa aouchiche <aouchiche.massinissa@gmail.com>
2021-12-02 09:29:51 +0100

    Changing the color of the chart and play with the seed

--- app/javascript/plugins/init_chart.js
+++ app/javascript/plugins/init_chart.js
@@ -11,9 +11,8 @@ const initChart = () =>{
           datasets: [{
               label: 'Job applications',
               data: JSON.parse(ctx.canvas.dataset.data),
-              label: "Minecrafty Sales",
               fill: true,
-              pointRadius: 3,
+              pointRadius: 4,
               font: 50,
               lineTension: 0.4,
               backgroundColor: [
@@ -24,9 +23,10 @@ const initChart = () =>{
                   'rgba(153, 102, 255, 0.2)'
               ],
               borderColor: [
+                  'grey',
+                 'rgba(255, 206, 86, 1)',
                   'rgba(255, 99, 132, 1)',
                   'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
                   'rgba(75, 192, 192, 1)',
                   'rgba(153, 102, 255, 1)',
                   'rgba(255, 159, 64, 1)'
